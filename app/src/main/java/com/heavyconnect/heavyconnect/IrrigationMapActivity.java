fa04fb0f073826d29aed3758675a9ea6ef32fde6
Martin Almaraz <martin.almaraz@gmail.com>
2015-09-19 11:49:22 -0700

    Ready to test Drawer

--- app/src/main/java/com/heavyconnect/heavyconnect/IrrigationMapActivity.java
+++ app/src/main/java/com/heavyconnect/heavyconnect/IrrigationMapActivity.java
@@ -3,7 +3,12 @@ package com.heavyconnect.heavyconnect;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 /**
  * Created by anon on 9/19/15.
@@ -22,6 +27,13 @@ public class IrrigationMapActivity extends Activity {
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
         // Set Adapter for the list view
-
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mFieldLocations));
+        // Set onClick listener
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"Hello HappyTown", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
