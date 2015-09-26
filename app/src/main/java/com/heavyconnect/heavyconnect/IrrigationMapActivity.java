15b9b220b31db71ab2aa95f44438f5e6d26dbbd4
Naelin Aquino <justnaelin@yahoo.com>
2015-09-26 12:59:41 -0700

    Implemented field tracing

--- app/src/main/java/com/heavyconnect/heavyconnect/IrrigationMapActivity.java
+++ app/src/main/java/com/heavyconnect/heavyconnect/IrrigationMapActivity.java
@@ -2,46 +2,44 @@ package com.heavyconnect.heavyconnect;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.heavyconnect.heavyconnect.entities.Manager;
 import com.heavyconnect.heavyconnect.resttasks.TaskCallback;
-import com.heavyconnect.heavyconnect.utils.Constants;
 import com.heavyconnect.heavyconnect.utils.StorageUtils;
 
+import java.util.ArrayList;
 
-/**
- * Created by anon on 9/19/15.
- */
-public class IrrigationMapActivity extends AppCompatActivity implements OnMapReadyCallback, ViewTreeObserver.OnGlobalLayoutListener, TaskCallback, GoogleMap.OnCameraChangeListener {
+public class IrrigationMapActivity extends AppCompatActivity implements TaskCallback,
+        GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener  {
+
     private String[] mFieldLocations;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
-    private SupportMapFragment mIrrigationMapFragment;
-
-    private FrameLayout mIrrigationMapContainer;
-
-    private GoogleMap mIrrigationMap;
-
     private ProgressDialog mProgress;
     private Manager mManager;
 
+    // Draw polygon
+    private GoogleMap mIrrigationMap;
+    private ArrayList<LatLng> mArrayPoints = null;
+    private boolean mMarkerClicked = false;
+    Polyline polylineOptions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,26 +51,30 @@ public class IrrigationMapActivity extends AppCompatActivity implements OnMapRea
             Toast.makeText(this, getString(R.string.map_loading), Toast.LENGTH_LONG).show();
             finish();
         }
-
-        MapsInitializer.initialize(this);
-
-        mIrrigationMapContainer = (FrameLayout) findViewById(R.id.irrigation_map_container);
-
-        mIrrigationMapFragment = SupportMapFragment.newInstance();
-        getSupportFragmentManager().beginTransaction().add(R.id.irrigation_map_container, mIrrigationMapFragment).commit();
-        mIrrigationMapFragment.getMapAsync(this);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mProgress = new ProgressDialog(this);
         mProgress.setTitle(null);
-        //mProgress.setMessage(getString(R.string.equip_list_loading));
+        mProgress.setMessage(getString(R.string.equip_list_loading));
         mProgress.setIndeterminate(true);
         mProgress.setCancelable(false);
 
         mFieldLocations = getResources().getStringArray(R.array.field_locations_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Draw polygon
+        mArrayPoints = new ArrayList<LatLng>();
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.irrigation_map);
+        mIrrigationMap = fm.getMap();
+
+        // Display zoom map
+        mIrrigationMap.setMyLocationEnabled(true);
+        mIrrigationMap.setOnMapClickListener(this);
+        mIrrigationMap.setOnMapLongClickListener(this);
+        mIrrigationMap.setOnMarkerClickListener(this);
+        mIrrigationMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         // Set Adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mFieldLocations));
@@ -83,36 +85,14 @@ public class IrrigationMapActivity extends AppCompatActivity implements OnMapRea
                 Toast.makeText(getApplicationContext(),"Hello HappyTown", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
-
-    @Override
-    public void onCameraChange(CameraPosition position) {
-        if (position.zoom > Constants.MAP_DEFAULT_MARKER_SMALL_ZOOM)
-            mIrrigationMap.animateCamera(CameraUpdateFactory.zoomTo(Constants.MAP_DEFAULT_MARKER_SMALL_ZOOM));
-    }
-
-    @Override
-    public void onGlobalLayout() {
-        mIrrigationMapContainer.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap irrigationMap) {
-        mIrrigationMap = irrigationMap;
-        mIrrigationMap.setOnCameraChangeListener(this);
-
-        ViewTreeObserver vto = mIrrigationMapContainer.getViewTreeObserver();
-        if(vto.isAlive()) {
-            vto.addOnGlobalLayoutListener(this);
-        }
-    }
-
     @Override
     public void onTaskFailed(int errorCode) {
         if(mProgress != null && mProgress.isShowing())
             mProgress.dismiss();
 
-        Toast.makeText(this, getString(R.string.map_load_failure), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, getString(R.string.irrigation_map_load_failure), Toast.LENGTH_LONG).show();
         finish();
     }
 
@@ -121,4 +101,40 @@ public class IrrigationMapActivity extends AppCompatActivity implements OnMapRea
         if(mProgress != null && mProgress.isShowing())
             mProgress.dismiss();
     }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if(mMarkerClicked == false) {
+            mIrrigationMap.addMarker(new MarkerOptions().position(latLng));
+            mArrayPoints.add(latLng);
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mIrrigationMap.clear();
+        mArrayPoints.clear();
+        mMarkerClicked = false;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if(mArrayPoints.get(0).equals(marker.getPosition())) {
+            countPolygonPoints();
+        }
+        return false;
+    }
+
+    public void countPolygonPoints() {
+        if(mArrayPoints.size() >= 3) {
+            mMarkerClicked = true;
+            PolygonOptions polygonOptions = new PolygonOptions();
+            polygonOptions.addAll(mArrayPoints);
+            polygonOptions.strokeColor(Color.BLUE);
+            polygonOptions.strokeWidth(7);
+            polygonOptions.fillColor(Color.YELLOW);
+            com.google.android.gms.maps.model.Polygon polygon = mIrrigationMap.addPolygon(polygonOptions);
+        }
+    }
+
 }
--- app/src/main/java/com/heavyconnect/heavyconnect/utils/Constants.java
+++ app/src/main/java/com/heavyconnect/heavyconnect/utils/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
     public static final int SOCKET_TIMEOUT = 30000;
 
     public static final float MAP_DEFAULT_MARKER_SMALL_ZOOM = 17.0f;
+
+    public static final float IRRIGATION_MAP_DEFAULT_MARKER_ZOOM = 100.0f;
 }
